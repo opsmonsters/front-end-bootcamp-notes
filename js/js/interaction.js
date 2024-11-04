a4711741e7c694be737c72cb8d6562b7a0e04687
Gershon Kumar <gershonkumar@live.com>
2024-11-04 11:22:59 +0530

    interaction & operations

---
+++ js/js/interaction.js
@@ -0,0 +1,11 @@
+// alert
+//alert("Testing out js interaction");
+
+//prompt
+let userInputName = prompt("What is your name ?");
+
+console.log(userInputName);
+
+//confirm
+let aboveConsentAge = confirm("Are you over the age of 18 ? ");
+console.log(aboveConsentAge);
---
+++ js/js/operator.js
@@ -0,0 +1,36 @@
+//Arithmetcic
+
+let x = 27;
+let y = 5;
+
+//ADD (+)
+console.log(">>>> Sum");
+let sum = x + y;
+console.log(sum);
+
+//Sub
+console.log(">>>> Diff");
+let diff = x - y;
+console.log(diff);
+
+//Mul
+console.log(">>>> Mul");
+let mul = x * y;
+console.log(mul);
+
+//Div
+console.log(">>>> Div");
+let quotient = x / y;
+let remainder = x % y;
+
+console.log(quotient);
+console.log(remainder);
+
+// String Operations
+
+let firstName = "Gershon";
+let lastName = "Kumar";
+
+let fullName = firstName + " " + lastName;
+
+console.log(fullName);
