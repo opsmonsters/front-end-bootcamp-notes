bc1ab8e7ee23c10ae26ad4c6726c93a1ea4ef90b
Gershon Kumar <gershonkumar@live.com>
2024-11-05 11:17:29 +0530

    js dir

---
+++ javascript/js/comparision.js
@@ -0,0 +1,16 @@
+let a = 10;
+let b = 20;
+let a_clone = "10";
+
+console.log(a > b);
+console.log(a < b);
+console.log(a >= b);
+console.log(a <= b);
+console.log(a == b);
+console.log(a != b);
+console.log(a == a_clone);
+console.log(a === a_clone);
+
+//undefined and null
+console.log(null == undefined); // do not use this
+console.log(null === undefined); // use this instead
