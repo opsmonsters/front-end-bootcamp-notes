aa5fb5d6cba588fafdfa271df7334f3c5c1d4e1b
Gershon Kumar <gershonkumar@live.com>
2024-11-06 11:37:07 +0530

    conditions

---
+++ javascript/js/condition.js
@@ -0,0 +1,30 @@
+// if
+
+if (true) {
+  //console.log("Inside True");
+}
+
+if (false) {
+  console.log("Inside False");
+}
+
+let isRaining = false;
+
+if (isRaining) {
+  console.log("Bring the umberlla");
+  console.log("Wear a rain coat");
+} else {
+  console.log("Wear a Sunglass");
+}
+
+console.log("Have a tea !!");
+
+let age = prompt("What is your age ? ");
+if (age >= 18) {
+  console.log("You can Vote");
+} else {
+  console.log("You can't Vote");
+}
+
+//one line if else (?)
+//age >= 18 ? console.log("Vote") : console.log("no Vote");
---
+++ javascript/js/logicalOperator.js
@@ -0,0 +1,15 @@
+// || -> OR
+console.log(true || true);
+console.log(true || false);
+console.log(false || true);
+console.log(false || false);
+
+// && -> AND
+console.log(true && true);
+console.log(true && false);
+console.log(false && true);
+console.log(false && false);
+
+// NOT
+console.log(!true);
+console.log(!false);
